e328f16d5fceb2d9f874211c3814826f5640c516
Davi Nickenig Cassitas Barbosa <devdavinickenig@gmail.com>
2023-01-13 15:34:32 -0300

    interação menu e Js finalizada.
    termino da interção entre os botoes do menu e JavaScript

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function toogleMenu() {
 }
 
 btnMobile.addEventListener('click', toogleMenu);
+
+
+
+const btnMobile__features = document.getElementById('btn-mobile__features');
+
+function toogleMenu__features () {
+    const nav = document.getElementById('nav__features');
+    nav.classList.toggle('active');
+}
+
+btnMobile__features.addEventListener('click', toogleMenu__features);
+
+const btnMobile__company = document.getElementById('btn-mobile__company');
+
+function toogleMenu__company () {
+    const nav = document.getElementById('nav__company');
+    nav.classList.toggle('active');
+}
+
+btnMobile__company.addEventListener('click', toogleMenu__company);
